7df72cbfbf65994633dcc5918a3a163c9858017c
Lawi Kiprotich <fleeklawi@gmail.com>
2023-12-18 23:09:45 +0300

    Initial Setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # Installed Apps 
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class RequestMech(models.Model):
+    car_owner = models.CharField(max_length = 50)
+    location = models.CharField(max_length=50)
+    car_issue = models.TextField()
+    priority = models.CharField(max_length=55)
+    expected_time = models.IntegerField()
+    expected_budget = models.IntegerField()
+    mech = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.car_owner} at {self.location}'
